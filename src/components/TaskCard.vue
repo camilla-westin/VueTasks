b6add6306c8bede2d665e5b0868a609df8604f50
Camilla Westin <camilla@webstin.se>
2024-01-29 10:46:04 +0100

    Card component

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  task: {
+    type: Object,
+  },
+});
+</script>
+<template>
+  <VCard
+    :title="task.title"
+    :subtitle="task.type"
+    :prepend-icon="task.type === 'bug' ? 'mdi-bug' : 'mdi-shape-square-plus'"
+    variant="tonal"
+    color="indigo-darken-3"
+    :append-icon="task.status === 'Done' ? 'mdi-check' : null"
+  >
+    <VCardText>{{ task.description }}</VCardText>
+  </VCard>
+</template>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { ref } from "vue";
 import Tasks from "@/data/tasks.json";
+import TaskCard from "@/components/TaskCard.vue";
 
 const tasks = ref(Tasks);
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-4">
-    <VCard v-for="task in tasks" :key="task.id">
-      <VCardTitle>{{ task.title }}</VCardTitle>
-      <VCardText>{{ task.status }}</VCardText>
-    </VCard>
-  </div>
+  <ul class="grid grid-cols-3 gap-4">
+    <li v-for="task in tasks" :key="task.id">
+      <TaskCard :task="task" />
+    </li>
+  </ul>
 </template>
