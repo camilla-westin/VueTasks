1770d0e5d6166bd3e1202843e80b4e14597e09e7
Camilla Westin <camilla@webstin.se>
2024-01-30 15:33:10 +0100

    Filtering by type

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import Tasks from "@/data/tasks.json";
 import TaskCard from "@/components/TaskCard.vue";
 import EditTask from "./EditTask.vue";
@@ -7,6 +7,9 @@ import EditTask from "./EditTask.vue";
 const tasks = ref(Tasks);
 const dialogStatus = ref(false);
 const editTaskData = ref(null);
+const showAllTasks = ref(true);
+const showBugs = ref(false);
+const showFeatures = ref(false);
 
 const deleteTask = (id) => {
   tasks.value = tasks.value.filter((task) => task.id !== id);
@@ -56,10 +59,69 @@ const editTask = (id) => {
 
   openDialog();
 };
+
+const handleCheckboxChange = (type) => {
+  if (type === "All") {
+    showAllTasks.value = true;
+    showBugs.value = false;
+    showFeatures.value = false;
+  } else if (type === "Bug") {
+    showAllTasks.value = false;
+    showBugs.value = true;
+    showFeatures.value = false;
+  } else if (type === "Feature") {
+    showAllTasks.value = false;
+    showBugs.value = false;
+    showFeatures.value = true;
+  }
+};
+
+const selectedFilterType = computed(() => {
+  if (showAllTasks.value) {
+    return "All";
+  } else if (showBugs.value) {
+    return "Bug";
+  } else if (showFeatures.value) {
+    return "Feature";
+  }
+});
+
+const filteredTasks = computed(() =>
+  tasks.value.filter((task) => {
+    const filterType = selectedFilterType.value;
+    return filterType === "All" || task.type === filterType;
+  })
+);
 </script>
 
 <template>
   <div class="flex justify-end items-center mb-6">
+    <VForm class="mr-6 flex items-center">
+      <VLabel>Filter by type</VLabel>
+      <VCheckboxBtn
+        v-model="showAllTasks"
+        label="All"
+        class="text-sm ml-3"
+        color="indigo"
+        @change="() => handleCheckboxChange('All')"
+        inline
+      ></VCheckboxBtn>
+      <VCheckboxBtn
+        v-model="showBugs"
+        label="Bug"
+        class="text-sm"
+        color="indigo"
+        @change="() => handleCheckboxChange('Bug')"
+        inline
+      ></VCheckboxBtn>
+      <VCheckboxBtn
+        v-model="showFeatures"
+        label="Feature"
+        class="text-sm"
+        @change="() => handleCheckboxChange('Feature')"
+        color="indigo"
+      ></VCheckboxBtn>
+    </VForm>
     <VBtn @click="openDialog" prepend-icon="mdi-plus">Add task</VBtn>
   </div>
   <VRow class="grid grid-cols-3 gap-4">
@@ -68,7 +130,9 @@ const editTask = (id) => {
 
       <ul>
         <li
-          v-for="task in tasks.filter((task) => task.status === 'To do')"
+          v-for="task in filteredTasks.filter(
+            (task) => task.status === 'To do'
+          )"
           :key="task.id"
           class="mb-4"
         >
@@ -85,7 +149,9 @@ const editTask = (id) => {
       <VDivider></VDivider>
       <ul>
         <li
-          v-for="task in tasks.filter((task) => task.status === 'In progress')"
+          v-for="task in filteredTasks.filter(
+            (task) => task.status === 'In progress'
+          )"
           :key="task.id"
           class="mb-4"
         >
@@ -102,7 +168,7 @@ const editTask = (id) => {
 
       <ul>
         <li
-          v-for="task in tasks.filter((task) => task.status === 'Done')"
+          v-for="task in filteredTasks.filter((task) => task.status === 'Done')"
           :key="task.id"
           class="mb-4"
         >
